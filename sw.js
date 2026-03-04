86916ed029cb7f55ff171eb84b9be3da94b31a26
mustaffapp <designingyourfabric@gmail.com>
2026-03-04 12:44:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const cacheName = '3s-sound-v1';
+const CACHE_NAME = '3s-sound-v2';
 const assets = [
   './',
   './index.html',
-  './myFont.otf',
+  './manifest.json',
   './3S_logo.png',
   './speaker_icon.png',
   './light_icon.png',
   './system_icon.png',
-  './con_icon.png',
   './settings_icon.png',
-  './note_bg.jpg'
+  './con_icon.png',
+  './note_bg.jpg',
+  './myFont.otf'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
